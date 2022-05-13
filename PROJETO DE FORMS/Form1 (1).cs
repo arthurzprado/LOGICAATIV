fc0cfca034d9675509f65e4c8ad08d3c1a1503f0
arthurzprado <105210787+arthurzprado@users.noreply.github.com>
2022-05-13 12:21:16 -0300

    Add files via upload

---
+++ PROJETO DE FORMS/Form1 (1).cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmAppCalc : Form
+    {
+        public FrmAppCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAC_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou AC");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou -");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou +");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Caixa de texto");
+        }
+
+        private void btnMaisMenos_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou +/-");
+        }
+
+        private void btnPorcentagem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou %");
+        }
+
+        private void btnDividido_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou ÷");
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou 7");
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou 8");
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou 9");
+        }
+
+        private void btnVezez_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou X");
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou 4");
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou 5");
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou 6");
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou 1");
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou 2");
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou 3");
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou 0");
+        }
+
+        private void btnVirgula_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou ,");
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Digitou =");
+        }
+
+        private void FrmAppCalc_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PROJETO DE FORMS/Form1.cs EXERCICIO 4 DE ABRIL.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EXERCICIO 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NUM1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "NUM2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(44, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "NUM3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(120, 202);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(448, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 73);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "SOMA";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(448, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 73);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "MÉDIA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(448, 282);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 73);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "PORCENTAGEM ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
